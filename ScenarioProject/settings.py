be44b5da85d5fa8f64b40c7607680e557b3cd7b8
Cosmin Nicolae <marnicos2006@yahoo.com>
2016-11-03 20:53:58 +0000

    Linked to MySQL Databases

--- ScenarioProject/settings.py
+++ ScenarioProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ScenarioProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'first_try',
+        'USER': 'dev1',
+        'PASSWORD': 'referenceme',
+        'HOST': 'test2.cjicdfrk5uxv.eu-west-1.rds.amazonaws.com',
+        'PORT': 3307
     }
 }
 
