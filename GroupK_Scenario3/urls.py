fb4861661ae1952cf2f2e619e1f0ceb894ae2407
tudornica <tudornica95@gmail.com>
2016-11-04 05:58:40 +0000

    aws working v1.0

--- GroupK_Scenario3/urls.py
+++ GroupK_Scenario3/urls.py
@@ -1,4 +1,4 @@
-"""ScenarioProject URL Configuration
+"""GroupK_Scenario3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- GroupK_Scenario3/wsgi.py
+++ GroupK_Scenario3/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ScenarioProject project.
+WSGI config for GroupK_Scenario3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ScenarioProject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GroupK_Scenario3.settings")
 
 application = get_wsgi_application()
